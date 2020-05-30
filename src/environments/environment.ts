d8b988acd068360769a0e088d578742819037ba5
yassine7319 <yassine.ne_7319@outlook.com>
2020-05-30 21:21:55 +0200

    firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+ firebase : {
+  apiKey: "AIzaSyDLEJJoYwWLG8N_QEpaekSiLneXZm6s-8c",
+  authDomain: "freelancinidb.firebaseapp.com",
+  databaseURL: "https://freelancinidb.firebaseio.com",
+  projectId: "freelancinidb",
+  storageBucket: "freelancinidb.appspot.com",
+  messagingSenderId: "350887955277",
+  appId: "1:350887955277:web:bc3844aa5b1eb510d58504",
+  measurementId: "G-7EE7X168Q8"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
